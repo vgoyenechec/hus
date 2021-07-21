11220231f0e0ace4929594f533c826e192887f39
Valentina <valegoye@gmail.com>
2021-07-21 13:47:20 -0500

    usuario

--- src/main/java/hus/censoCamas/model/Usuario.java
+++ src/main/java/hus/censoCamas/model/Usuario.java
@@ -1,34 +1,61 @@
 package hus.censoCamas.model;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity(name = "GENUSUARIO")
 public class Usuario {
     @Id
-    @Column(updatable = false,nullable = false)
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    @Column(name = "OID",updatable = false,nullable = false)
+    private Integer id;
+    @Column(name = "GENROL")
+    private int rol;
+    @Column(name = "USUNOMBRE")
     private String usuario;
-    private String nombreUsuario;
-    private String tipoUsuario;
+    @Column(name = "USUDESCRI")
+    private String descripcion;
+    @Column(name = "USUCLAVE")
     private String clave;
+    @Column(name = "USUESTADO")
+    private int estado;
+    @Column(name = "USUULTAUT")
+    private LocalDateTime ultimaAutenticacion;
+    @Column(name = "USUFALLO1")
+    private int fallosClave;
+    @Column(name = "USUAUTENT")
+    private boolean autenticado;
+    @Transient
+    private String[] permisos;
+
 
     public Usuario(){}
 
-    public Usuario(int id, String usuario, String nombreUsuario, String tipoUsuario, String clave) {
-        this.id = id;
+    public Usuario(int rol, String usuario, String descripcion, String clave, int estado, LocalDateTime ultimaAutenticacion, int fallosClave, boolean autenticado, String[] permisos) {
+        this.rol = rol;
         this.usuario = usuario;
-        this.nombreUsuario = nombreUsuario;
-        this.tipoUsuario = tipoUsuario;
+        this.descripcion = descripcion;
         this.clave = clave;
+        this.estado = estado;
+        this.ultimaAutenticacion = ultimaAutenticacion;
+        this.fallosClave = fallosClave;
+        this.autenticado = autenticado;
+        this.permisos = permisos;
     }
 
-    public int getIdUsuario() {
+    public Integer getId() {
         return id;
     }
 
-    public void setIdUsuario(int id) {
+    public void setId(Integer id) {
         this.id = id;
+    }
+
+    public int getRol() {
+        return rol;
+    }
+
+    public void setRol(int rol) {
+        this.rol = rol;
     }
 
     public String getUsuario() {
@@ -39,20 +66,12 @@ public class Usuario {
         this.usuario = usuario;
     }
 
-    public String getNombreUsuario() {
-        return nombreUsuario;
+    public String getDescripcion() {
+        return descripcion;
     }
 
-    public void setNombreUsuario(String nombreUsuario) {
-        this.nombreUsuario = nombreUsuario;
-    }
-
-    public String getTipoUsuario() {
-        return tipoUsuario;
-    }
-
-    public void setTipoUsuario(String tipoUsuario) {
-        this.tipoUsuario = tipoUsuario;
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
     }
 
     public String getClave() {
@@ -63,13 +82,43 @@ public class Usuario {
         this.clave = clave;
     }
 
-    @Override
-    public String toString() {
-        return "Usuario{" +
-                "usuario='" + usuario + '\'' +
-                ", nombreUsuario='" + nombreUsuario + '\'' +
-                ", tipoUsuario='" + tipoUsuario + '\'' +
-                ", clave='" + clave + '\'' +
-                '}';
+    public int getEstado() {
+        return estado;
+    }
+
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
+
+    public LocalDateTime getUltimaAutenticacion() {
+        return ultimaAutenticacion;
+    }
+
+    public void setUltimaAutenticacion(LocalDateTime ultimaAutenticacion) {
+        this.ultimaAutenticacion = ultimaAutenticacion;
+    }
+
+    public int getFallosClave() {
+        return fallosClave;
+    }
+
+    public void setFallosClave(int fallosClave) {
+        this.fallosClave = fallosClave;
+    }
+
+    public boolean isAutenticado() {
+        return autenticado;
+    }
+
+    public void setAutenticado(boolean autenticado) {
+        this.autenticado = autenticado;
+    }
+
+    public String[] getPermisos() {
+        return permisos;
+    }
+
+    public void setPermisos(String[] permisos) {
+        this.permisos = permisos;
     }
 }
