c40ded2e38752d8542685cce5774615df8ab641c
Valentina <valegoye@gmail.com>
2021-10-26 14:46:29 -0500

    remove files

--- src/main/java/hus/censoCamas/repo/EgresoRepo.java
+++
@@ -1,9 +0,0 @@
-package hus.censoCamas.repo;
-
-import hus.censoCamas.model.Egreso;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EgresoRepo extends JpaRepository<Egreso, Integer> {
-    void deleteByIdEgreso(int idEgreso);
-}
-
--- src/main/java/hus/censoCamas/repo/EstanciaRepo.java
+++
@@ -1,7 +0,0 @@
-package hus.censoCamas.repo;
-
-import hus.censoCamas.model.Estancia;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EstanciaRepo extends JpaRepository<Estancia, Integer> {
-}
