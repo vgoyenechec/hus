be3123710d3e19d3433220c7f024d8aa9b59675f
Valentina <valegoye@gmail.com>
2021-11-30 16:54:23 -0500

    clase Message

---
+++ src/main/java/hus/censoCamas/exception/Message.java
@@ -0,0 +1,17 @@
+package hus.censoCamas.exception;
+
+public class Message {
+    private String message;
+
+    public Message(String message) {
+        this.message = message;
+    }
+
+    public String getMensaje() {
+        return message;
+    }
+
+    public void setMensaje(String message) {
+        this.message = message;
+    }
+}
