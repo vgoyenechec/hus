67f412fb738a3210274ff3c00c3aef6d1fff4c67
Valentina <valegoye@gmail.com>
2022-01-17 11:32:38 -0500

    remove attributes

--- src/main/java/hus/censoCamas/model/Paciente.java
+++ src/main/java/hus/censoCamas/model/Paciente.java
@@ -12,10 +12,6 @@ public class Paciente implements Serializable{
     private Integer id;
     @Column(name = "PACNUMDOC")
     private String documento;
-    @Column(name = "PACTIPDOC")
-    private int tipoDocumento;
-    @Column(name = "PACEXPEDI")
-    private String lugarExpedicion;
     @Column(name = "PACPRINOM")
     private String prinom;
     @Column(name = "PACSEGNOM")
@@ -24,26 +20,18 @@ public class Paciente implements Serializable{
     private String priape;
     @Column(name = "PACSEGAPE")
     private String segape;
-    @Column(name = "GPAFECNAC")
-    private LocalDateTime fechaNacimiento;
-    @Column(name = "GPASEXPAC")
-    private int sexo;
 
 
     public Paciente() {
     }
 
-    public Paciente(Integer id, String documento, int tipoDocumento, String lugarExpedicion, String prinom, String segnom, String priape, String segape, LocalDateTime fechaNacimiento, int sexo) {
+    public Paciente(Integer id, String documento, String prinom, String segnom, String priape, String segape) {
         this.id = id;
         this.documento = documento;
-        this.tipoDocumento = tipoDocumento;
-        this.lugarExpedicion = lugarExpedicion;
         this.prinom = prinom;
         this.segnom = segnom;
         this.priape = priape;
         this.segape = segape;
-        this.fechaNacimiento = fechaNacimiento;
-        this.sexo = sexo;
     }
 
     public int getId() {
@@ -54,14 +42,6 @@ public class Paciente implements Serializable{
 
     public String getDocumento() {
         return documento;
-    }
-
-    public int getTipoDocumento() {
-        return tipoDocumento;
-    }
-
-    public String getLugarExpedicion() {
-        return lugarExpedicion;
     }
 
     public String getPrimerNombre() {
@@ -78,30 +58,8 @@ public class Paciente implements Serializable{
 
     public String getSegundoApellido() { return segape; }
 
-    public LocalDateTime getFechaNacimiento() {
-        return fechaNacimiento;
-    }
-
-    public int getSexo() {
-        return sexo;
-    }
-
     public String getNombreCompleto(){
         return getPrimerNombre()+' '+getSegundoNombre()+' '+getPrimerApellido()+' '+getSegundoApellido();
     }
 
-    @Override
-    public String toString() {
-        return "Paciente{" +
-                "documento='" + documento + '\'' +
-                ", tipoDocumento=" + tipoDocumento +
-                ", lugarExpedicion='" + lugarExpedicion + '\'' +
-                ", Primer Nombre='" + prinom + '\'' +
-                ", Segundo Nombre='" + segnom + '\'' +
-                ", Primer Apellido='" + priape + '\'' +
-                ", Segundo Apellido='" + segape + '\'' +
-                ", fechaNacimiento=" + fechaNacimiento +
-                ", sexo='" + sexo + '\'' +
-                '}';
-    }
 }
